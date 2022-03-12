8eddba57bd823a3854fd091e553357a7d06fa98e
root <root@thibaut.localdomain>
2022-03-12 18:01:45 +0100

    [ADD] fonction qui creer un affichage du tableau

--- my_sort.php
+++ my_sort.php
@@ -5,13 +5,53 @@ function array_swap(&$array,$swap_a,$swap_b){
     list($array[$swap_a],$array[$swap_b]) = array($array[$swap_b],$array[$swap_a]);
  }
 
+ function isTabEmpty($array)
+ {
+     $isEmpty = true;
+
+     foreach($array as $value)
+     {
+        if ($value != 0)
+            $isEmpty = false;
+     }
+     return $isEmpty;
+ }
+
+ function printTab($array)
+ {
+     $printableArray = $array;
+     foreach($array as $value)
+     {
+         echo $value;
+     }
+     echo "\n";
+     while (isTabEmpty($printableArray) == false)
+     {
+        foreach($printableArray as &$value)
+        {
+            if ($value > 0)
+            {
+                echo "█";
+                $value = $value - 1;
+            }
+            elseif ($value == 0)
+            {
+                echo " ";
+            }
+        }
+        echo "\n";
+     }
+ }
+
 function my_sort($array)
 {
     $i = 0;
 
     while($i < count($array) -1)
     {
-        //print_r($array);
+        system('clear');
+        printTab($array);
+        usleep(50000);
         if($array[$i] > $array[$i+1])
         {
             array_swap($array, $i, ($i + 1));
@@ -19,8 +59,7 @@ function my_sort($array)
         }
         $i++;
     }
-    print_r($array);
 }
 
-$ar = [3,5,1,8,3];
-my_sort([3,5,1,8,3]);
+$ar = [1,2,3,9,5,6,7,8,4];
+my_sort($ar);
